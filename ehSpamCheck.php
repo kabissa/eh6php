a586b3320acb51133233d32d14bcb7aefad47cef
Erik Hommel <hommel@ee-atwork.nl>
2014-11-15 09:17:39 +0100

    work in progress, completing functions

--- ehSpamCheck.php
+++ ehSpamCheck.php
@@ -27,38 +27,38 @@ require_once 'CRM/Core/Config.php';
 $config = CRM_Core_Config::singleton( );
 
 $suspects = array();
-$groupMembers = array();
-$countFlagged = 0;
-$countTrashed = 0;
+$group_members = array();
+$count_flagged = 0;
+$count_trashed = 0;
 
-$contacts = getContactsToBeChecked();
+$contacts = get_contacts_to_be_checked();
 foreach ($contacts as $contact) {
-  if (isParticipant($contact['contact_id']) == FALSE) {
-    if (isInSpecialGroups($contact['contact_id']) == TRUE) {
-      $groupmembers[] = $contact['contact_id'];
+  if (is_participant($contact['contact_id']) == FALSE) {
+    if (is_in_special_groups($contact['contact_id']) == TRUE) {
+      $group_members[] = $contact['contact_id'];
     } 
-    if (isSuspectName($contact['display_name']) == TRUE || 
-      isSuspectEmail($contact['email']) == TRUE) {
+    if (is_suspect_name($contact['display_name']) == TRUE || 
+      is_suspect_email($contact['email']) == TRUE) {
       $suspects[] = $contact['contact_id'];
     }
   }
 }
 unset($contacts);
-foreach ($suspects as $suspectContactId) {
-  if (in_array($groupMembers)) {
-    processFlag($suspectContactId);
-    $countFlagged++;
+foreach ($suspects as $suspect_contact_id) {
+  if (in_array($group_members)) {
+    process_flag($suspect_contactId);
+    $count_flagged++;
   } else {
-    processTrash($suspectContactId);
-    $countTrashed++;
+    process_trash($suspect_contact_id);
+    $count_trashed++;
   }
 }
-echo "<p>".$countTrashed." contacts trashed, ".$countFlagged.
+echo "<p>".$count_trashed." contacts trashed, ".$count_flagged.
   " flagged as ToBeSpamChecked</p>";
 /**
  * Function to get contacts to be checked
  */
-function getContactsToBeChecked() {
+function get_contacts_to_be_checked() {
   $params = array(
     'version' => 3,
     'is_deleted' => 0,
@@ -74,36 +74,62 @@ function getContactsToBeChecked() {
 /**
  * Function to check if the contact has been participant at any event
  */
-function isParticipant($contactId) {
+function is_participant($contact_id) {
+  $query = 'SELECT COUNT(*) as count_participant FROM civicrm_participant WHERE '
+    . 'contact_id = %1';
+  $params = array(1 => array($contact_id, 'Positive'));
+  $dao = CRM_Core_DAO::executeQuery($query, $params);
+  if ($dao->fetch()) {
+    if ($dao->count_participant > 0) {
+      return TRUE;
+    }
+  }
   return FALSE;
 }
 /**
  * Function to check if the contact is member of one of the special groups
  */
-function isInSpecialGroups($contactId) {
+function is_in_special_groups($contact_id) {
+  $special_groups = explode(':', EH_SPECIALGROUPS);
+  $params = array(
+    'version' => 3,
+    'contact_id' => $contact_id,
+    'options' => array('limit' => 99999));
+  $contact_groups = civicrm_api('GroupContact', 'Get', $params);
+  foreach ($contact_groups['values'] as $contact_group) {
+    if (in_array($contact_group['group_id'], $special_groups)) {
+      return TRUE;
+    }
+  }
   return FALSE;
 }
 /**
  * Function to check if the name has a digit in it and is therefore suspect
  */
-function isSuspectName($name) {
+function is_suspect_name($name) {
+  $name_length = strlen($name);
+  for ($start = 0; $start < $name_length; $start++) {
+    if (is_numeric(substr($name, $start, 1))) {
+      return TRUE;
+    }
+  }
   return FALSE;
 }
 /**
  * Function to check if the email address is suspect
  */
-function isSuspectEmail($email) {
+function is_suspect_email($email) {
   return FALSE;
 }
 /**
  * Function to flag contacts as ToBeSpamChecked
  */
-function processFlag($contactId) {
+function process_flag($contact_id) {
   
 }
 /**
  * Function to trash contacts
  */
-function processTrash($contactId) {
+function process_trash($contact_id) {
   
 }
